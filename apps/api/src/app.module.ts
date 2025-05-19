694e53c56ba98c7c1e259f176dd15e09d263e31c
Carlos Nexans <carlos.nexans@clau.com>
2025-05-19 11:24:02 -0300

    serve static files

--- apps/api/src/app.module.ts
+++ apps/api/src/app.module.ts
@@ -1,13 +1,28 @@
 import { Module } from '@nestjs/common';
 import { CacheModule } from '@nestjs/cache-manager';
+import { ServeStaticModule } from '@nestjs/serve-static';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import * as path from 'path';
 
 @Module({
   imports: [
     CacheModule.register({
       ttl: 600000, // 10 minutes in milliseconds
       max: 100, // maximum number of items in cache
+    }),
+    // eslint-disable-next-line
+    ServeStaticModule.forRootAsync({
+      useFactory: () => [
+        {
+          rootPath: path.resolve(process.env.IMAGES_PATH || ''),
+          serveRoot: '/media',
+          serveStaticOptions: {
+            fallthrough: false,
+            index: false,
+          },
+        },
+      ],
     }),
   ],
   controllers: [AppController],
